0e0d8dda04ee29012905480267148798ee6c35a9
Annabelle <109839853+annabelleaaron@users.noreply.github.com>
2023-10-04 12:49:14 +1300

    convert a string to a number

--- 2023/jskata.js
+++ 2023/jskata.js
@@ -5,10 +5,23 @@ function sum(numbers) { // js function .reduce() - return the sum of all the ele
 }
 // my solution
 function sum (numbers) {
-    "use strict";
-    let n = 0;
-    for (let i = 0; i < numbers.length; i++) {
-      n += numbers[i];
-    }
-    return n;    
+  "use strict";
+  let n = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    n += numbers[i];
+  }
+  return n;    
 };
+
+
+// CONVERT A STRING TO A NUMBER
+var stringToNumber = function(str){
+  return parseInt(str); // will still parse the number even if it included other characters eg. '123a' will return 123
+} // parseInt(value, radix) - takes a string and returns the first number and converts it from the given radix into base10
+var stringToNumber = function(str){
+  return Number(str); // faster than parseInt(), but if number included other characters eg. '123a' it will return NaN
+} // Number() - tries to cast the input as a number
+// my solution
+var stringToNumber = function(str){
+  return +str; // if the operand is not a number, converts it into a number. similar to Number()
+} // numeric conversion - unary
--- 2023/pykata.py
+++ 2023/pykata.py
@@ -7,3 +7,15 @@ def sum_array(a):
     for x in a:
         n += x
     return n
+
+
+# CONVERT A STRING TO A NUMBER
+def string_to_number(s): # try:... except:...
+    try:
+        return int(s)
+    except (ValueError):
+        return "Input is not valid"
+string_to_number = int # using an alias for the function name: int eg. string_to_number("1234") to int("1234")
+# my solution
+def string_to_number(s):
+    return int(s)
